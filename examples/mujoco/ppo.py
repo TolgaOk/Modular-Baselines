3c67b3521ffb87a5a5c45b493e93b190049e4dc8
Tolga Ok <tolgaokk@gmail.com>
2022-06-13 15:10:34 +0300

    Add Separete network

--- examples/mujoco/ppo.py
+++ examples/mujoco/ppo.py
@@ -9,59 +9,13 @@ from stable_baselines3.common.vec_env.subproc_vec_env import SubprocVecEnv
 from stable_baselines3.common.logger import Logger, configure
 
 from modular_baselines.algorithms.ppo.torch_policy import TorchPPOPolicy
+from modular_baselines.networks.network import SharedFeatureNetwork, SeparateFeatureNetwork
 from modular_baselines.algorithms.ppo.ppo import PPO
 from modular_baselines.loggers.basic import InitLogCallback, LogRolloutCallback, LogLossCallback
 from modular_baselines.utils.annealings import Coefficient, LinearAnnealing
 
 
-class Policy(torch.nn.Module, TorchPPOPolicy):
-
-    def __init__(self, observation_space: Space, action_space: Space, hidden: int = 128, lr: float = 1e-4):
-        super().__init__()
-
-        self.observation_space = observation_space
-        self.action_space = action_space
-
-        self.in_size = observation_space.shape[0]
-        self.out_size = action_space.shape[0]
-
-        self.lr = lr
-        self.hidden_size = hidden
-        self.feature = torch.nn.Sequential(
-            torch.nn.Linear(self.in_size, hidden),
-            torch.nn.LayerNorm(hidden),
-            torch.nn.RReLU(),
-            torch.nn.Linear(hidden, hidden),
-            torch.nn.LayerNorm(hidden),
-            torch.nn.RReLU(),
-        )
-
-        self.policy_head = torch.nn.Linear(hidden, self.out_size * 2)
-
-        torch.nn.init.xavier_normal_(self.policy_head.weight, 0.01)
-        self.value_head = torch.nn.Linear(hidden, 1)
-
-        self._optimizer = torch.optim.Adam(self.parameters(), lr=lr)
-
-    def forward(self, state: torch.Tensor, *args
-                ) -> Tuple[torch.distributions.Normal, Optional[torch.Tensor], torch.Tensor]:
-        """ Return policy distribution and value
-        Args:
-            state (torch.Tensor): State tensor
-        Returns:
-            Tuple[torch.distributions.Normal, torch.Tensor]: Normal 
-                policy distribution and value
-        """
-        feature = self.feature(state)
-        logits = self.policy_head(feature)
-        value = self.value_head(feature)
-
-        mean, std_logit = logits.split(self.out_size, dim=1)
-        std = torch.nn.functional.softplus(std_logit)
-        dist = torch.distributions.Normal(loc=mean, scale=std + 0.1)
-        dist = torch.distributions.independent.Independent(dist, 1)
-
-        return dist, None, value
+class Policy(SeparateFeatureNetwork, TorchPPOPolicy):
 
     @property
     def device(self) -> Device:
@@ -117,22 +71,22 @@ def setup(env_name: str, hyperparameters: Dict[str, Any], seed: int):
 
 
 ppo_mujoco_walker2d_hyperparameters = dict(
-    lr=1e-4,
+    lr=3e-4,
     n_envs=16,
     n_steps=2048,
     ent_coef=1e-4,
     vf_coef=0.5,
-    gamma=0.995,
-    gae_lambda=0.90,
-    epochs=5,
-    clip_value=LinearAnnealing(0.2, 0.01, int(3_000_000 / 2048 / 16)),
+    gamma=0.99,
+    gae_lambda=0.95,
+    epochs=10,
+    clip_value=LinearAnnealing(0.2, 0.2, int(3_000_000 / 2048 / 16)),
     batch_size=64,
     max_grad_norm=1.0,
-    total_timesteps=3_000_000,
+    total_timesteps=5_000_000,
     log_interval=1,
     device="cpu",
 )
 
 
 if __name__ == "__main__":
-    agent = setup("Walker2d-v4", ppo_mujoco_walker2d_hyperparameters, seed=1003)
+    agent = setup("Swimmer-v4", ppo_mujoco_walker2d_hyperparameters, seed=1003)
--- modular_baselines/networks/network.py
+++ modular_baselines/networks/network.py
@@ -51,6 +51,56 @@ class SharedFeatureNetwork(torch.nn.Module):
         return dist, None, value
 
 
+class SeparateFeatureNetwork(torch.nn.Module):
+
+    def __init__(self, observation_space: Space, action_space: Space, policy_hidden_size: int = 128, value_hidden_size: int = 200, lr: float = 0.0001):
+        super().__init__()
+        self.observation_space = observation_space
+        self.action_space = action_space
+
+        self.in_size = observation_space.shape[0]
+        self.out_size = action_space.shape[0]
+        if isinstance(action_space, Box):
+            self.out_size = self.out_size * 2
+
+        self.lr = lr
+        self.policy_hidden_size = policy_hidden_size
+        self.value_hidden_size = value_hidden_size
+
+        self.policy_net = torch.nn.Sequential(
+            torch.nn.Linear(self.in_size, policy_hidden_size),
+            torch.nn.Tanh(),
+            torch.nn.Linear(policy_hidden_size, policy_hidden_size),
+            torch.nn.Tanh(),
+            torch.nn.Linear(policy_hidden_size, self.out_size)
+        )
+        self.value_net = torch.nn.Sequential(
+            torch.nn.Linear(self.in_size, value_hidden_size),
+            torch.nn.RReLU(),
+            torch.nn.Linear(value_hidden_size, value_hidden_size),
+            torch.nn.RReLU(),
+            torch.nn.Linear(value_hidden_size, 1)
+        )
+
+        torch.nn.init.xavier_normal_(self.policy_net[-1].weight, 0.01)
+        self._optimizer = torch.optim.Adam(self.parameters(), lr=lr)
+
+    def forward(self, state: torch.Tensor, *args
+                ) -> Tuple[torch.distributions.Normal, Optional[torch.Tensor], torch.Tensor]:
+        """ Return policy distribution and value
+        Args:
+            state (torch.Tensor): State tensor
+        Returns:
+            Tuple[torch.distributions.Normal, Optional[torch.Tensor], torch.Tensor]: 
+                policy distribution, None, value
+        """
+        logits = self.policy_net(state)
+        value = self.value_net(state)
+
+        dist = get_dist(logits, self.action_space)
+        return dist, None, value
+
+
 def get_dist(logits: torch.Tensor, action_space: Union[Box, Discrete]
              ) -> Union[torch.distributions.Normal, torch.distributions.Categorical]:
     if isinstance(action_space, Box):
