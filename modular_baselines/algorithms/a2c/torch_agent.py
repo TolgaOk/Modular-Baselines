b4f243467e2395d8216bfb54886c2072dd33cef5
Tolga Ok <tolgaokk@gmail.com>
2022-07-16 22:48:52 +0300

    Update Network: Seperate policy params from policy distribution.

--- modular_baselines/algorithms/a2c/torch_agent.py
+++ modular_baselines/algorithms/a2c/torch_agent.py
@@ -22,7 +22,8 @@ class TorchA2CAgent(TorchAgent):
         with torch.no_grad():
             th_observation = self.to_torch(observation).float()
 
-            policy_dist, _ = self.policy(th_observation)
+            policy_params, _ = self.policy(th_observation)
+            policy_dist = self.policy.dist(policy_params)
             th_action = policy_dist.sample()
             if isinstance(self.action_space, Discrete):
                 th_action = th_action.unsqueeze(-1)
@@ -36,12 +37,13 @@ class TorchA2CAgent(TorchAgent):
              sample["next_observation"][:, -1])]
         return th_obs, th_action, th_next_obs
 
-    def prepare_rollout(self, sample: np.ndarray, gamma: float, gae_lambda: float) -> List[torch.Tensor]:
+    def replay_rollout(self, sample: np.ndarray, gamma: float, gae_lambda: float) -> List[torch.Tensor]:
         env_size, rollout_size = sample["observation"].shape[:2]
         th_obs, th_action, th_next_obs = self.rollout_to_torch(
             sample)
 
-        policy_dist, th_flatten_values = self.policy(self.flatten_time(th_obs))
+        policy_params, th_flatten_values = self.policy(self.flatten_time(th_obs))
+        policy_dist = self.policy.dist(policy_params)
 
         tf_values = th_flatten_values.reshape(env_size, rollout_size, 1)
         _, th_next_value = self.policy(th_next_obs)
@@ -72,7 +74,7 @@ class TorchA2CAgent(TorchAgent):
         for param_group in self.optimizer.param_groups:
             param_group['lr'] = lr
 
-        advantages, returns, th_action, policy_dist, th_values = self.prepare_rollout(
+        advantages, returns, th_action, policy_dist, th_values = self.replay_rollout(
             sample, gamma=gamma, gae_lambda=gae_lambda)
 
         log_probs = policy_dist.log_prob((th_action))
--- modular_baselines/networks/network.py
+++ modular_baselines/networks/network.py
@@ -4,9 +4,9 @@ import numpy as np
 from gym.spaces import Space, Box, Discrete
 
 
-class SharedFeatureNetwork(torch.nn.Module):
+class BaseNetwork(torch.nn.Module):
 
-    def __init__(self, observation_space: Space, action_space: Space, hidden_size: int = 128):
+    def __init__(self, observation_space: Space, action_space: Union[Box, Discrete]):
         super().__init__()
 
         self.observation_space = observation_space
@@ -16,6 +16,26 @@ class SharedFeatureNetwork(torch.nn.Module):
         self.out_size = action_space.shape[0]
         if isinstance(action_space, Box):
             self.out_size = self.out_size * 2
+
+    def dist(self, parameters: torch.Tensor) -> Union[torch.distributions.Normal, torch.distributions.Categorical]:
+        if isinstance(self.action_space, Box):
+            mean, std_logit = parameters.split(parameters.shape[-1] // 2, dim=1)
+            std = torch.nn.functional.softplus(std_logit)
+            dist = torch.distributions.Normal(loc=mean, scale=std + 0.05)
+            dist = torch.distributions.independent.Independent(dist, 1)
+            return dist
+
+        if isinstance(self.action_space, Discrete):
+            dist = torch.distributions.Categorical(logits=parameters)
+            return dist
+
+        raise ValueError(f"Action space: {self.action_space.__class__.__name__} is not supported!")
+
+
+class SharedFeatureNetwork(BaseNetwork):
+
+    def __init__(self, observation_space: Space, action_space: Space, hidden_size: int = 128):
+        super().__init__(observation_space, action_space)
 
         self.hidden_size = hidden_size
         self.feature = torch.nn.Sequential(
@@ -33,33 +53,25 @@ class SharedFeatureNetwork(torch.nn.Module):
         self.value_head = torch.nn.Linear(hidden_size, 1)
 
     def forward(self, state: torch.Tensor, *args
-                ) -> Tuple[torch.distributions.Normal, torch.Tensor]:
+                ) -> Tuple[torch.Tensor, torch.Tensor]:
         """ Return policy distribution and value
         Args:
             state (torch.Tensor): State tensor
         Returns:
-            Tuple[torch.distributions.Normal, torch.Tensor]:
+            Tuple[torch.Tensor, torch.Tensor]:
                 policy distribution, value
         """
         feature = self.feature(state)
         logits = self.policy_head(feature)
         value = self.value_head(feature)
 
-        dist = get_dist(logits, self.action_space)
-        return dist, value
+        return logits, value
 
 
-class SeparateFeatureNetwork(torch.nn.Module):
+class SeparateFeatureNetwork(BaseNetwork):
 
     def __init__(self, observation_space: Space, action_space: Space, policy_hidden_size: int = 64, value_hidden_size: int = 64):
-        super().__init__()
-        self.observation_space = observation_space
-        self.action_space = action_space
-
-        self.in_size = observation_space.shape[0]
-        self.out_size = action_space.shape[0]
-        if isinstance(action_space, Box):
-            self.out_size = self.out_size * 2
+        super().__init__(observation_space, action_space)
 
         self.policy_hidden_size = policy_hidden_size
         self.value_hidden_size = value_hidden_size
@@ -80,33 +92,23 @@ class SeparateFeatureNetwork(torch.nn.Module):
         )
 
     def forward(self, state: torch.Tensor, *args
-                ) -> Tuple[torch.distributions.Normal, torch.Tensor]:
+                ) -> Tuple[torch.Tensor, torch.Tensor]:
         """ Return policy distribution and value
         Args:
             state (torch.Tensor): State tensor
         Returns:
-            Tuple[torch.distributions.Normal, torch.Tensor]:
+            Tuple[torch.Tensor, torch.Tensor]:
                 policy distribution, None, value
         """
         logits = self.policy_net(state)
         value = self.value_net(state)
-
-        dist = get_dist(logits, self.action_space)
-        return dist, value
+        return logits, value
 
 
-class LSTMSeparateNetwork(torch.nn.Module):
+class LSTMSeparateNetwork(BaseNetwork):
 
     def __init__(self, observation_space: Space, action_space: Space, policy_hidden_size: int = 64, value_hidden_size: int = 64):
-        super().__init__()
-
-        self.observation_space = observation_space
-        self.action_space = action_space
-
-        self.in_size = observation_space.shape[0]
-        self.out_size = action_space.shape[0]
-        if isinstance(action_space, Box):
-            self.out_size = self.out_size * 2
+        super().__init__(observation_space, action_space)
 
         self.policy_hidden_size = policy_hidden_size
         self.value_hidden_size = value_hidden_size
@@ -138,7 +140,7 @@ class LSTMSeparateNetwork(torch.nn.Module):
 
         policy_features = torch.tanh(self.policy_in_layer(state))
         policy_hx, policy_cx = self.policy_lstm(policy_features, (policy_hx, policy_cx))
-        policy_logits = self.policy_out(policy_hx)
+        policy_logit = self.policy_out(policy_hx)
 
         value_features = torch.tanh(self.value_in_layer(state))
         value_hx, value_cx = self.value_lstm(value_features, (value_hx, value_cx))
@@ -146,8 +148,7 @@ class LSTMSeparateNetwork(torch.nn.Module):
 
         hidden_states = dict(value_hx=value_hx, value_cx=value_cx,
                              policy_hx=policy_hx, policy_cx=policy_cx)
-        dist = get_dist(policy_logits, self.action_space)
-        return dist, value, hidden_states
+        return policy_logit, value, hidden_states
 
 
 def linear_layer_init(layer: torch.nn.Linear, std: float = np.sqrt(2), bias_const: float = 0.0):
@@ -163,19 +164,3 @@ def lstm_init(layer: torch.nn.LSTMCell, std: float = 1, bias_const: float = 0.0)
         elif "weight" in name:
             torch.nn.init.orthogonal_(param, std)
     return layer
-
-
-def get_dist(logits: torch.Tensor, action_space: Union[Box, Discrete]
-             ) -> Union[torch.distributions.Normal, torch.distributions.Categorical]:
-    if isinstance(action_space, Box):
-        mean, std_logit = logits.split(logits.shape[-1] // 2, dim=1)
-        std = torch.nn.functional.softplus(std_logit)
-        dist = torch.distributions.Normal(loc=mean, scale=std + 0.05)
-        dist = torch.distributions.independent.Independent(dist, 1)
-        return dist
-
-    if isinstance(action_space, Discrete):
-        dist = torch.distributions.Categorical(logits=logits)
-        return dist
-
-    raise ValueError(f"Action space: {action_space.__class__.__name__} is not supported!")
