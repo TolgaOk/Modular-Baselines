6f1f8b78873ebbd8c013c99a0691251bc644810d
TolgaOk <okt@itu.edu.tr>
2023-04-09 14:23:03 +0300

    Update version

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Modular-Baselines is a library designed for Reinforcement Learning (RL) research
 
 To keep the codebase easy to read and maintain, we've implemented each algorithm within a single script using the components provided in Modular-Baselines. Our approach emphasizes duck-typing instead of inheritance, which helps us minimize the dependency between components and algorithms.
 
-### **Implemented Algorithms**
+### **Algorithms**
 
 | Algorithm |  <img src="https://raw.githubusercontent.com/google/jax/main/images/jax_logo_250px.png" width = 64px alt="logo"></img> | <img src="https://pytorch.org/assets/images/pytorch-logo.png" width = 50px  height = 50px alt="logo"></img> |
 |:-----:|:---------:|:---------:|
--- setup.py
+++ setup.py
@@ -3,15 +3,13 @@ from setuptools import setup, find_packages
 setup(
     # Metadata
     name="modular_baselines",
-    version="0.2.0",
+    version="0.2.0-alpha",
     author="Tolga Ok",
     author_email="okt@itu.edu.tr",
     url="",
-    description="A modular RL library build on Stable-baselines3",
-    long_description=("Framework agnostic modular components for RL research with Pytorch and"
-                      " JAX policy implementations. Some of the common utilities are imported"
-                      " from SB3. Likewise, the structure is quite similar that of SB3 but with"
-                      "a modular approach."),
+    description="A modular RL library",
+    long_description=("Framework agnostic modular components for RL research written in Numpy, "
+                      "Pytorch, and JAX. Includes, examples, logging mechanism, and vega visualizations"),
     license="MIT",
 
     # Package info
